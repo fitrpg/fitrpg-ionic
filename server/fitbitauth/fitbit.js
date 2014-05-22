60dd0a3fc5e3b3e2511dce4202fa3aed03ae0f63
Amira Anuar <amiraanuar@gmail.com>
2014-05-22 15:30:26 -0700

    Incorporate fitbit credentials into fitbitauth file and added basic app middleware requirements to config file, including passport, oauth, and session

---
+++ server/fitbitauth/fitbit.js
@@ -0,0 +1,6 @@
+var passport = require('passport');
+var FitbitStrategy = require('passport-fitbit').Strategy;
+
+// Passport credential configs, sign up
+var FITBIT_CONSUMER_KEY = '8cda22173ee44a5bba066322ccd5ed34';
+var FITBIT_CONSUMER_SECRET = '12beae92a6da44bab17335de09843bc4';
--- server/main/config.js
+++ server/main/config.js
@@ -4,7 +4,11 @@ var bodyParser    = require('body-parser'),
     cookieParser  = require('cookie-parser'),
     middle        = require('./middleware'),
     mongoose      = require('mongoose'),
-    morgan        = require('morgan');
+    morgan        = require('morgan'),
+    methodOverride= require('method-override'),
+    OAuth         = require('oauth').OAuth,
+    passport      = require('passport');
+
 
 
 mongoose.connect(process.env.DB_URL || 'mongodb://localhost/myApp');
@@ -21,4 +25,15 @@ module.exports = exports = function (app, express, routers) {
   app.use('/note' , routers.NoteRouter);
   app.use(middle.logError);
   app.use(middle.handleError);
+  app.use(methodOverride());
+  /* 
+   * passport.initialize() middleware is required to initialize Passport. 
+   * Because this application uses persistent login sessions, passport.session() 
+   * middleware must also be used. If enabling session support, express.session()
+   * must be used BEFORE passport.session() to ensure that the login is 
+   * restored in the correct order.
+   */
+  app.use(session({secret: 'amiraconormatt', maxAge: 360*5}));
+  app.use(passport.initialize());
+  app.use(passport.session());
 };
