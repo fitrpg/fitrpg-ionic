0413e4a2b3ab486d0e4d17789190a46325fe5bae
Matthew Gutierrez <fatchicken007@gmail.com>
2014-05-22 18:58:38 -0700

    Require jawbone js file

--- server/main/app.js
+++ server/main/app.js
@@ -3,6 +3,7 @@
 var cookieParser = require('cookie-parser');
 var express      = require('express');
 var request      = require('request');
+var passport     = require('passport');
 
 var app          = express();
 var NoteRouter   = express.Router();
@@ -18,5 +19,7 @@ require('./config.js')(app, express, routers);
 require('../note/note_routes.js')(NoteRouter);
 require('../user/user_routes.js')(UserRouter);
 
+
+require('../jawbone/jawbone.js')(app, passport);
 
 module.exports = exports = app;
