a805a47af7d535918428be11f5e590dc949ca3b1
Matthew Gutierrez <fatchicken007@gmail.com>
2014-05-22 17:32:44 -0700

    Add module exports to pass in app and passport from app.js

--- server/jawbone/jawbone.js
+++ server/jawbone/jawbone.js
@@ -1,48 +1,51 @@
-var JawboneStrategy = require('passport-jawbone').Strategy;
-var jawbone = require('jawbone-up');
+module.exports = exports = function(app,passport) {
+  var JawboneStrategy = require('passport-jawbone').Strategy;
+  var jawbone = require('jawbone-up');
 
-var JAWBONE_CLIENT_ID = 'FdNCRVpJNW4';
-var JAWBONE_CLIENT_SECRET = '9bfce3019e13d6fd775904455df5540873403e7c';
+  var JAWBONE_CLIENT_ID = 'FdNCRVpJNW4';
+  var JAWBONE_CLIENT_SECRET = '9bfce3019e13d6fd775904455df5540873403e7c';
 
-var jawbone = new JawboneStrategy({
-  clientID: JAWBONE_CLIENT_ID,
-  clientSecret: JAWBONE_CLIENT_SECRET,
-  callbackURL: "https://fitbitgamify.azurewebsites.net/FitbitRPG"
-  },
-  function(accessToken,refreshToken,profile,done) {
-    console.log('Token: ', accessToken);
-    console.log('TokenSecret: ', refreshToken);
-    console.log(profile);
+  var jawbone = new JawboneStrategy({
+    clientID: JAWBONE_CLIENT_ID,
+    clientSecret: JAWBONE_CLIENT_SECRET,
+    callbackURL: "https://fitbitgamify.azurewebsites.net/FitbitRPG"
+    },
+    function(accessToken,refreshToken,profile,done) {
+      console.log('Token: ', accessToken);
+      console.log('TokenSecret: ', refreshToken);
+      console.log(profile);
 
-    var options = {
-      access_token: accessToken,
-      client_secret: JAWBONE_CLIENT_SECRET
+      var options = {
+        access_token: accessToken,
+        client_secret: JAWBONE_CLIENT_SECRET
+      }
+      var up = jawbone(options);
+
+      process.nextTick(function(){
+        return done(null,profile);
+      })
     }
-    var up = jawbone(options);
+  )
 
-    process.nextTick(function(){
-      return done(null,profile);
-    })
-  }
-)
+  passport.use(jawbone);
 
-passport.use(jawbone);
+  app.get('/auth/jawbone',passport.authenticate('jawbone'));
 
-app.get('/auth/jawbone',passport.authenticate('jawbone'));
-
-app.get('/auth/jawbone/callback',
-  passport.authenticate('jawbone',{ failureRedirect:'/login'}),
-  function(req,res){
-    console.log('Successful authentication!');
-    res.redirect('/');
-});
-
-app.get('/jawbone/pubsub', function(req,res){
-  console.log('webhook received');
-});
-
-app.get('/jawbone/update', function(req,res){
-  up.webhook.create('http://127.0.0.1:9000/jawbone/pubsub',function(err,body){
-    console.log(JSON.parse(body).data);
+  app.get('/auth/jawbone/callback',
+    passport.authenticate('jawbone',{ failureRedirect:'/login'}),
+    function(req,res){
+      console.log('Successful authentication!');
+      res.redirect('/');
   });
-});
+
+  app.get('/jawbone/pubsub', function(req,res){
+    console.log('webhook received');
+  });
+
+  app.get('/jawbone/update', function(req,res){
+    up.webhook.create('http://127.0.0.1:9000/jawbone/pubsub',function(err,body){
+      console.log(JSON.parse(body).data);
+    });
+  });
+
+};
