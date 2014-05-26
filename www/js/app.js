ac1ed3dd7ad50c917495dc4fd208dad35d256176
Hack Reactor Students <accounts@hackreactor.com>
2014-05-26 12:48:50 -0700

    Client side authentication complete

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ui.bootstrap'])
+angular.module('starter', ['ionic', 'starter.controllers','app.auth' ,'starter.services', 'ui.bootstrap'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
