92d66d7f52f7f5e53b0e17ea58796430a9d1677c
Matthew Gutierrez <fatchicken007@gmail.com>
2014-05-27 13:51:04 -0700

    Add functions inside each view controller to handle UI events

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['LocalStorageModule'])
+angular.module('starter.controllers', ['LocalStorageModule','ionic'])
 
 .controller('DashCtrl', function($scope, User) {
   $scope.user = User;
@@ -14,6 +14,7 @@ angular.module('starter.controllers', ['LocalStorageModule'])
     $scope.user.attributes[attr]++;
     $scope.user.attributes.skillPoints--;
     if (attr === 'vitality') {
+      // change char class from warrior to user class
       $scope.user.attributes.hp = util.updateHp($scope.user.attributes.hp,'warrior');
       $scope.user.attributes.maxHp = util.updateHp($scope.user.attributes.maxHp,'warrior');
     }
@@ -35,17 +36,24 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 
   $scope.equip = function(slot){
     console.log(slot);
-    // send to inventory
   };
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
+.controller('FriendsCtrl', function($scope, Friends, User) {
+  // add DashCtrl or pull updated data from database
+  $scope.user = User;
   $scope.friends = Friends.all();
+
+  $scope.requestBattle = function(id) {
+    // update $scope.battle to reflect status of pending with friend
+    // post to database to update friends battle status
+  };
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+// not currently being used
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
 
 .controller('AddFriendsCtrl', function($scope, AddFriends) {
   $scope.friends = AddFriends.all();
@@ -56,8 +64,56 @@ angular.module('starter.controllers', ['LocalStorageModule'])
   $scope.filter = 'weapon'
 })
 
-.controller('InventoryDetailCtrl', function($scope, $stateParams, Inventory) {
+.controller('InventoryDetailCtrl', function($scope, $stateParams, Inventory, User) {
+  // add DashCtrl or pull updated data from database
+  $scope.user = User;
   $scope.inventoryItem = Inventory.get($stateParams.inventoryId);
+
+  $scope.addClass = function(attr) {
+    if (attr > 0) {
+      return 'text-green';
+    } else {
+      return 'text-red';
+    }
+  };
+
+  $scope.sellItem = function() {
+    $scope.user.attributes.gold = $scope.user.attributes.gold + $scope.inventoryItem.salePrice;
+    // remove from inventory
+  };
+
+  $scope.equipItem = function() {
+    if ($scope.inventoryItem.type === 'weapon') {
+      if ($scope.inventoryItem.size === 1) {
+        if ($scope.user.attributes.weapon1 === undefined) {
+          $scope.user.attributes.weapon1 = $scope.inventoryItem.name;
+          // set item status to equipped
+        } else if ($scope.user.attributes.weapon2 === undefined) {
+          $scope.user.attributes.weapon2 = $scope.inventoryItem.name;
+          // set item status to equipped
+        }
+      } else if ($scope.inventoryItem.size === 2) {
+        if ($scope.user.attributes.weapon1 === undefined && $scope.user.attributes.weapon2 === undefined) {
+          $scope.user.attributes.weapon1 = $scope.inventoryItem.name;
+          $scope.user.attributes.weapon2 = $scope.inventoryItem.name;
+          //set item status to equipped
+        }
+      }
+    }
+  };
+
+  $scope.useItem = function() {
+    $scope.user.attributes.hp += $scope.inventoryItem.restore;
+    // subtract quantity from inventory -> remove if quantity = 0
+  }
+
+  $scope.checkType = function() {
+    if ($scope.inventoryItem.type === 'potion') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 })
 
 .controller('ShopCtrl', function($scope, Shop) {
@@ -65,41 +121,228 @@ angular.module('starter.controllers', ['LocalStorageModule'])
   $scope.filter = 'weapon'
 })
 
-.controller('ShopDetailCtrl', function($scope, $stateParams, Shop) {
+.controller('ShopDetailCtrl', function($scope, $stateParams, Shop, User) {
+  // add DashCtrl or pull updated data from database
+  $scope.user = User;
   $scope.shopItem = Shop.get($stateParams.shopId);
+
+  $scope.addClass = function(attr) {
+    if (attr > 0) {
+      return 'text-green';
+    } else {
+      return 'text-red';
+    }
+  };
+
+  $scope.buyItem = function() {
+    $scope.user.attributes.gold = $scope.user.attributes.gold - $scope.shopItem.buyPrice;
+    // add to inventory
+  };
+
+  $scope.checkType = function() {
+    if ($scope.shopItem.type === 'potion') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 })
 
-.controller('BattleCtrl', function($scope, Battle) {
-  $scope.battles = Battle.all();
+.controller('BattleCtrl', function($scope, Battle, User) {
+  $scope.user = User;
+
+  $scope.cancelBattle = function(id) {
+    // remove battle from $scope.user.battles
+    // update database for both players
+  };
+
+  $scope.startBattle = function(id) {
+    // get user attributes from database
+    // use game logic to determine winner of battle
+    // post battle results to database for both players
+  };
+
+  $scope.pending = function() {
+    $scope.isPending = true;
+    $scope.battles = Battle.all();
+  };
+
+  $scope.history = function(id) {
+    // get data from battle history database
+    // replace $scope.battles with results
+    $scope.isPending = false;
+    $scope.battles = [];
+  };
+
+  $scope.pending();
 })
 
-.controller('BattleDetailCtrl', function($scope, $stateParams, Battle) {
-  $scope.battle = Battle.get($stateParams.battleId);
+// not currently being used
+// .controller('BattleDetailCtrl', function($scope, $stateParams, Battle) {
+//   $scope.battle = Battle.get($stateParams.battleId);
+// })
+
+.controller('SoloMissionCtrl', function($scope, SoloMissions, User) {
+  var user = User;
+
+  $scope.new = function() {
+    $scope.soloMissions = [];
+    var allSoloMissions = SoloMissions.all();
+    //need to filter missions that are complete or greater than current user level
+    for (var i=0; i<allSoloMissions.length; i++) {
+      var soloMission = allSoloMissions[i];
+      if (soloMission.level <= user.attributes.level) {
+        $scope.soloMissions.push(soloMission);
+      }
+    }
+  };
+
+  $scope.complete = function() {
+    // completed missions in user database
+    $scope.soloMissions = [];
+  };
+
+  $scope.new();
 })
 
-.controller('SoloMissionCtrl', function($scope, SoloMissions) {
-  $scope.soloMissions = SoloMissions.all();
-})
-
-.controller('SoloMissionDetailCtrl', function($scope, $stateParams, SoloMissions) {
+.controller('SoloMissionDetailCtrl', function($scope, $stateParams, SoloMissions, $ionicPopup, $timeout, $q) {
   $scope.soloMission = SoloMissions.get($stateParams.missionId);
+
+  $scope.difficulty = function(num) {
+    if ( num <= $scope.soloMission.difficulty ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.showAlert = function() {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Mission Started',
+      template: 'You are waging war against the forces of evil...',
+      okText: 'Continue'
+    });
+    alertPopup.then(function(res) {
+      $scope.showResults();
+    })
+  };
+
+  $scope.showResults = function() {
+    //do game logic to see if you win
+      // if win give exp and gold
+        // display 'You are victorious!'
+        // mark mission as complete
+      // if lose display on screen
+        // diplay 'You are no match for [boss name]'
+
+    var alertPopup = $ionicPopup.alert({
+      title: 'Mission Results',
+      template: 'You are victorious!',
+      okText: 'Close'
+    });
+    alertPopup.then(function(res) {
+    })
+  };
+
+  $scope.startMission = function() {
+    if ($scope.soloMission.type === 'boss') {
+      $scope.showAlert();
+    } else {
+      //if solo mission is quest
+        // save start time of quest
+        // if accomplish goals within duration
+          // display 'You have completed quest!'
+        // if not
+          // display 'Your quest has failed.'
+    }
+  };
 })
 
-.controller('VersusMissionCtrl', function($scope, VersusMissions) {
-  $scope.versusMissions = VersusMissions.all();
+.controller('VersusMissionCtrl', function($scope, VersusMissions, User) {
+  var user = User;
+
+  $scope.new = function() {
+    $scope.isComplete = false;
+    $scope.versusMissions = VersusMissions.all();
+  };
+
+  $scope.complete = function() {
+    // completed missions in user database
+    $scope.isComplete = true;
+    $scope.versusMissions = [];
+  };
+
+  $scope.new();
+
 })
 
-.controller('VersusMissionDetailCtrl', function($scope, $stateParams, VersusMissions) {
+.controller('VersusMissionDetailCtrl', function($scope, $stateParams, VersusMissions, Friends) {
   $scope.versusMission = VersusMissions.get($stateParams.missionId);
+  $scope.friends = Friends.all();
+
+  $scope.selectFriends = function() {
+    $scope.showFriends = true;
+  };
+
+  $scope.addFriend = function(id) {
+    if ($scope.versusMission.friends.length < $scope.versusMission.size) {
+      $scope.versusMission.friends.push(id);
+    }
+    console.log($scope.versusMission.friends);
+  };
+
+  $scope.removeFriend = function(id) {
+    for (var i=0; i<$scope.versusMission.friends.length; i++) {
+      var friend = $scope.versusMission.friends[i];
+      if (friend === id) {
+        $scope.versusMission.friends.splice(i,1);
+      }
+    }
+  };
+
+  $scope.inParty = function(id) {
+    for (var i=0; i<$scope.versusMission.friends.length; i++) {
+      var friend = $scope.versusMission.friends[i];
+      if (friend === id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  $scope.requestMission = function() {
+    // push notify friends of mission
+  };
+
+  // server side could check if all friends accepted mission
+  // if all friend accept mission, push notify all friends that mission is starting
+
+  $scope.checkMissionStatus = function() {
+    // check start time and duration
+    // if past duration check participants data
+    // may need to check time intervals for when someone won?
+  };
+
+  $scope.checkMissionStatus();
 })
 
-.controller('LeaderboardCtrl', function($scope, Leaderboard) {
-  $scope.leaderboard = Leaderboard.all();
+.controller('LeaderboardCtrl', function($scope, Leaderboard, Friends) {
+
+  $scope.all = function() {
+    $scope.leaderboard = Leaderboard.all();
+  };
+
+  $scope.friends = function() {
+    $scope.leaderboard = Friends.all();
+  };
+
+  $scope.all();
 })
 
-.controller('LeaderboardDetailCtrl', function($scope, $stateParams, Leaderboard) {
-  $scope.leader = Leaderboard.get($stateParams.leaderId);
-})
+// not currently being used
+// .controller('LeaderboardDetailCtrl', function($scope, $stateParams, Leaderboard) {
+//   $scope.leader = Leaderboard.get($stateParams.leaderId);
+// })
 
 .controller('HelpCtrl', function($scope, $stateParams, Leaderboard) {
 })
