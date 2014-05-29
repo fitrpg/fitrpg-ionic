9f7744bafcbb73810550bd72f9ea1bb9e79329dc
Matthew Gutierrez <fatchicken007@gmail.com>
2014-05-28 22:41:57 -0700

    Add d3 directive donut chart to display battle win/losses

--- www/js/app.js
+++ www/js/app.js
@@ -191,4 +191,51 @@ angular.module('starter', ['ionic', 'starter.controllers','app.auth' ,'starter.s
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/character');
 
+})
+
+.directive('donutChart', function ($parse) {
+  var directiveDefinitionObject = {
+     restrict: 'E',
+     replace: false,
+     scope: {data: '=chartData'},
+     link: function (scope, element, attrs) {
+
+      var width = 200,
+          height = 200,
+          radius = Math.min(width, height) / 2;
+
+      var color = d3.scale.ordinal()
+          .range(["#1ba39c","#e74c3c","#abb7b7"]);
+
+      var arc = d3.svg.arc()
+          .outerRadius(radius - 50)
+          .innerRadius(radius - 10);
+
+      var pie = d3.layout.pie()
+          .sort(null)
+          .value(function(d) { return d; });
+
+      var svg = d3.select(element[0]).append("svg")
+          .attr("width", width)
+          .attr("height", height)
+        .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        var g = svg.selectAll(".arc")
+            .data(pie(scope.data))
+          .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) { return color(d.data); });
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .style("text-anchor", "middle")
+            .text(function(d) { return d.data; });
+     }
+  };
+  return directiveDefinitionObject;
 });
